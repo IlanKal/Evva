f42790b8a5f98b4ea1d8be1550dd87b00be89e3e
tehilaBaron <12tehila12@gmail.com>
2025-03-30 18:24:12 +0300

    routing and create chat component

---
+++ Frontend/Evva/src/app/modules/chat/components/chat/chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat',
+  standalone: true,
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent {
+  // הוסיפי כאן את הלוגיקה של הקומפוננטה
+}
