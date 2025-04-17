85450deb77ce9bfccfbb47dbd594b42823b84a3b
Ilan Kalantarov <elan000223@gmail.com>
2025-04-17 16:14:09 +0300

    update app + server

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -7,41 +7,6 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-
-// Routes tabels DB
-import userRoutes from './routes/userRoutes';
-app.use("/api", userRoutes);
-
-import eventRoutes from './routes/eventRoutes';
-app.use("/api", eventRoutes);
-
-import eventSupplierRoutes from './routes/eventSupplierRoutes';
-app.use('/api', eventSupplierRoutes);
-
-import guestRoutes from "./routes/guestRoutes";
-app.use("/api", guestRoutes);
-
-import photographerRoutes from "./routes/photographerRoutes"; // נשתמש לא לרישום אלא להצגה\חיפוש\עידכון של ספקים ספציפיים
-app.use("/api", photographerRoutes);
-
-import locationRoutes from "./routes/locationRoutes"; // נשתמש לא לרישום אלא להצגה\חיפוש\עידכון של ספקים ספציפיים
-app.use("/api", locationRoutes);
-
-import cateringRoutes from "./routes/cateringRoutes"; // נשתמש לא לרישום אלא להצגה\חיפוש\עידכון של ספקים ספציפיים
-app.use("/api", cateringRoutes);
-
-import speakerRoutes from "./routes/speakerRoutes"; // נשתמש לא לרישום אלא להצגה\חיפוש\עידכון של ספקים ספציפיים
-app.use("/api", speakerRoutes);
-
-import registerSupplierRoute from "./routes/registerSupplierRoutes"; //יוצר ספק חדש ומכניס גם לטבלת suppliers וגם לסוג הספציפי (DJ, צלם וכו')
-app.use("/api", registerSupplierRoute);
-
-import filterSuppliersRoutes from "./routes/filterSuppliersRoutes";
-app.use("/api", filterSuppliersRoutes);
-
-
-// Start the server and connect to the database
-
 const startServer = async () => {
   try {
     await sequelize.authenticate();
@@ -59,14 +24,4 @@ const startServer = async () => {
   }
 };
 
-
-//אם אתה רוצה לראות את הפילטור אפשר מפה
-//import { filterRelevantSuppliers } from "./services/filterRelevantSuppliers";
-
-
-// פה את שולח לפונקציה את מספר הREQUEST
-//filterRelevantSuppliers(1);
-
-// Run the server
 startServer();
-
