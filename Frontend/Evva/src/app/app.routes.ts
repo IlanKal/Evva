f3414218c90b961a9ff9e4688b8f3d31a0f80108
tehilaBaron <12tehila12@gmail.com>
2025-04-20 19:16:57 +0300

    login

--- Frontend/Evva/src/app/app.routes.ts
+++ Frontend/Evva/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ChatComponent } from './modules/chat/components/chat/chat.component';
+import { LoginComponent } from './modules/login/components/login/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'chat', component: ChatComponent }
 ];
