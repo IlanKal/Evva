23e884e67f795d3ed38fcded2bcc540df61445da
Ilan Kalantarov <elan000223@gmail.com>
2025-06-04 19:59:52 +0300

    Logout in Frontend

--- Frontend/Evva/src/app/modules/shared/header/header.component.ts
+++ Frontend/Evva/src/app/modules/shared/header/header.component.ts
@@ -7,12 +7,16 @@ import { IUser } from '../../../models/IUser';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 
-
-
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, RouterModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
@@ -25,5 +29,16 @@ export class HeaderComponent {
     const type = localStorage.getItem('type');
     return type === 'supplier' ? '/profile/supplier' : '/profile/user';
   }
-  
+
+  logout(): void {
+    
+    const confirmed = confirm('Are you sure you want to logout?');
+    if (!confirmed) return;
+    
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('type');
+    this.router.navigate(['/login']);
+  }
 }
